9a192e16d034e4fee7865c934329f2c19387430e
khs <hyeongs2323@gmail.com>
2023-04-02 18:35:07 +0900

    styles: link add

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/" legacyBehavior>
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/" ? styles.active : "",
+          ].join(" ")}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about" legacyBehavior>
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
           About
         </a>
       </Link>
